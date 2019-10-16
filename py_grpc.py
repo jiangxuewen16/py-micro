fc1af9af77bd9df52ff35fb9517bde7e575ac82d
DESKTOP-094KS21\jiang <445251692@qq.com>
2019-10-16 17:32:18 +0800

    py_micro

---
+++ py_grpc.py
@@ -0,0 +1,46 @@
+import time
+from collections import namedtuple
+
+import grpc
+from concurrent import futures
+
+_HOST = '127.0.0.1'
+_PORT = '8111'
+
+server_register = namedtuple('server_register', 'add_server server_class')
+
+
+class Producer(object):
+    _ONE_DAY_IN_SECONDS = 60 * 60 * 24
+
+    service_list = []
+
+    def __init__(self, max_workers=4):
+        self.grpc_server = grpc.server(futures.ThreadPoolExecutor(max_workers))
+
+    def set_service(self, service_list: list):
+        for service in service_list:
+            # todo: 这里做服务注册， 怎么做服务健康检查 register
+            # user_pb2_grpc.add_UserServicer_to_server(User(), grpcServer)
+            service['add_server'](service['server_class'], self.grpc_server)
+        return self
+
+    def start(self, host=None, port=None, ):
+        host = host or _HOST
+        port = port or _PORT
+        self.grpc_server.add_insecure_port(host + ':' + port)
+        self.grpc_server.start()
+        try:
+            while True:
+                time.sleep(self._ONE_DAY_IN_SECONDS)
+        except KeyboardInterrupt:
+            self.grpc_server.stop(0)
+
+
+class Consumer(object):
+    def __init__(self):
+        # todo:从cousul拿取服务地址 address + port
+        self.address = '127.0.0.1'
+        self.port = '8111'
+        pass
+
---
+++ setup.py
@@ -0,0 +1,69 @@
+# -*- coding:utf-8 -*-
+try:
+    from setuptools import setup, find_packages
+except:
+    from distutils.core import setup
+from codecs import open
+from os import path
+
+# 版本号
+VERSION = '0.0.1'
+
+# 发布作者
+AUTHOR = "jiangxuewen"
+
+# 邮箱
+AUTHOR_EMAIL = "445251692@qq.com"
+
+# 项目网址
+URL = "https://github.com/jiangxuewen16/py-micro"
+
+# 项目名称
+NAME = "py_micro"
+
+# 项目简介
+DESCRIPTION = "基于grpc的Python微服务"
+
+# LONG_DESCRIPTION为项目详细介绍，这里取README.md作为介绍
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+# 搜索关键词
+KEYWORDS = "py_micro micro"
+
+# 发布LICENSE
+LICENSE = "MIT"
+
+# 包
+PACKAGES = ["py_micro"]
+
+# 具体的设置
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+
+    ],
+    # 指定控制台命令
+    entry_points={
+        'console_scripts': [
+            'demo = demo:main',  # pip安装完成后可使用demo命令调用demo下的main方法
+        ],
+    },
+    keywords=KEYWORDS,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    license=LICENSE,
+    packages=PACKAGES,
+    install_requires=['grpcio', 'python-consul', 'grpcio-tools'],  # 依赖的第三方包
+    include_package_data=True,
+    zip_safe=True,
+)
